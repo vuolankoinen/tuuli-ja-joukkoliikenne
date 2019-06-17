9589605d6852bffa8662468f3c8583ef760d3f28
vuolankoinen <matti.palomaki@helsinki.fi>
2019-06-17 21:42:27 +0300

    Trying the new disruptions API

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # tuuli-ja-joukkoliikenne
-Using FMI data and HSL data to explore the effect of weather on reliability of trams vs. buses in the Helsinki public transport.
+The idea was to use FMI data and HSL data to explore the effect of weather on reliability of trams vs. buses in the Helsinki public transport. The disruptions in timetables are, however, only available in real time, so forming a dataset to study is unfeasible.
 
 Information on the HSL tram and bus location API is at
 https://digitransit.fi/en/developers/apis/4-realtime-api/vehicle-positions/
--- hae-pysakkidata.py
+++ hae-pysakkidata.py
@@ -1,11 +1,70 @@
-import paho.mqtt.subscribe as subscribe
+#import paho.mqtt.subscribe as subscribe
 
-viestit = subscribe.simple("/hfp/v1/journey/ongoing/tram/+/+/1008/#", hostname="mqtt.hsl.fi", msg_count=30, retained=True)
-#1004 = nelosratikka, 1008 = kasiratikka
+#viestit = subscribe.simple("/hfp/v1/journey/ongoing/tram/+/+/1008/#", hostname="mqtt.hsl.fi", msg_count=30, retained=True)
+# #1004 = nelosratikka, 1008 = kasiratikka
 
-for msg in viestit:
-    print("%s \n----\n %s" % (msg.topic, msg.payload))
-    #x=1
-
+#for msg in viestit:
+#    print("%s \n----\n %s" % (msg.topic, msg.payload))
 
 import requests
+from datetime import datetime # datetime(c)
+import time
+
+def query_HSY(q):
+    d = requests.post('https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql', json={'query': q})
+    return(d.json())
+
+dataHSY = query_HSY('''
+{
+  cancelledTripTimes(
+    feeds: ["HSL"]
+    minDate: "2016-04-08"
+    maxDate: "2019-06-21"
+  ) {
+    scheduledDeparture
+    serviceDay
+     trip {
+      routeShortName
+      directionId
+      route {
+        longName
+      }
+    }
+    }
+}''')
+
+#print(dataHSY)
+
+dataHSY = dataHSY['data']['cancelledTripTimes']
+
+for hmm in dataHSY:
+    # print('Pv {}, reitti {}, suunta {}'.format(time.ctime(hmm['serviceDay']), hmm['trip']['routeShortName'], hmm['trip']['directionId']))
+    print('Pv {}, reitti {}, suunta {}'.format(time.strftime('%Y %B %d', time.localtime(hmm['serviceDay'])), hmm['trip']['routeShortName'], hmm['trip']['directionId']))
+
+
+
+
+#  {
+#    cancelledTripTimes(
+#      feeds: ["HSL"]
+#    ) {
+#      scheduledDeparture
+#      serviceDay
+#      trip {
+#        gtfsId
+#        tripHeadsign
+#        routeShortName
+#        directionId
+#        pattern {
+#          code
+#          name
+#        }
+#        route {
+#          gtfsId
+#          longName
+#        }
+#      }
+#      realtimeState
+#      headsign
+#    }
+#  }
